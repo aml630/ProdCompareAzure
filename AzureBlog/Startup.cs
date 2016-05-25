81305720d41b869e95a69747967a8f80802638fd
alex <alex.m.larson@gmail.com>
2016-05-25 14:18:13 -0700

    Add project files.

---
+++ AzureBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureBlog.Startup))]
+namespace AzureBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
