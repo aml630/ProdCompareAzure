4eb353fcfb36c7e66a31fdf6da73f98752e5abd8
alex <alex.m.larson@gmail.com>
2016-06-13 19:22:35 -0700

    add ThreeJS to background of home

--- AzureBlog/Scripts/scripts.js
+++ AzureBlog/Scripts/scripts.js
@@ -10,3 +10,51 @@ $(".learnMore").click(function () {
 
     $(".alignCenter").nextAll(".moreResources").eq(0).slideToggle();
 })
+
+
+
+function next(x) {
+
+    var total = $(".productParent").children().length;
+    for (var i = 0; i< total; i = i+x)
+    {
+        //debugger;
+        if( $(".productParent").children().eq(i).is(":visible")===true)
+        {
+            console.log('true')
+            $(".productParent").children().eq(i).hide();
+            $(".productParent").children().eq(i + x).show();
+            if (i+1 === total)
+            {
+                $(".productParent").children().eq(0).show();
+            }
+
+
+    
+
+            return "working";
+        }
+    }
+    $(".productParent").children().eq(0).show();
+
+}
+
+
+$(".next").click(function () {
+    next(1);
+});
+
+
+$('.prev').click(function () {
+
+    next(-1);
+
+});
+
+window.onload = next(1);
+
+setInterval(function () {
+    next(1);
+}, 5000);
+
+
